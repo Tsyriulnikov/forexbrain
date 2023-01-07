53a86a6cf3d42f9c97ae114ffb81f955fadae00e
Tsyriulnikov <max_djvu@mail.ru>
2023-01-07 20:41:32 +0300

    service ver2

--- src/app/store/actions/main-fetch.actions.ts
+++ src/app/store/actions/main-fetch.actions.ts
@@ -2,12 +2,16 @@ import {createAction, props} from "@ngrx/store";
 
 export const GET_HISTORICAL_CURRENCIES_LIST = '[PokemonList] Get Pokemon List'
 export const GET_HISTORICAL_CURRENCIES_LIST_SUCCESS = '[PokemonList] Get Pokemon List Success'
-
+export const GET_HISTORICAL_CURRENCIES_LIST_FAILURE = '[PokemonList] Get Pokemon List Failure'
 
 export const getHistoricalCurrenciesListAction = createAction(
   GET_HISTORICAL_CURRENCIES_LIST,
 )
 export const getHistoricalCurrenciesListSuccessAction = createAction(
   GET_HISTORICAL_CURRENCIES_LIST_SUCCESS,
-  // props<{ response: PokemonResponse }>()
+  props<{ response: any }>()
+)
+export const getHistoricalCurrenciesListFailure = createAction(
+  GET_HISTORICAL_CURRENCIES_LIST_FAILURE,
+  props<any>()
 )
--- src/app/store/effects/main-fetch,effects.ts
+++ src/app/store/effects/main-fetch,effects.ts
@@ -1,16 +1,31 @@
 import {Injectable} from "@angular/core";
 import {MainFetchService} from "../../services/main-fetch.service";
-import {createEffect, ofType} from "@ngrx/effects";
-import {exhaustMap} from "rxjs";
+import {Actions, createEffect, ofType} from "@ngrx/effects";
+import {catchError, exhaustMap, map, of} from "rxjs";
+import {
+  getHistoricalCurrenciesListAction, getHistoricalCurrenciesListFailure,
+  getHistoricalCurrenciesListSuccessAction,
+} from "../actions/main-fetch.actions";
 
 @Injectable()
 export class MainFetchEffects {
   constructor(
-    // private actions$: Actions,
+    private actions$: Actions,
     private mainFetchService: MainFetchService
   ) {
   }
-
+  getHistoricalCurrenciesList$ = createEffect(() =>
+    this.actions$.pipe(
+      ofType(getHistoricalCurrenciesListAction),
+      exhaustMap(action =>
+        this.mainFetchService.getHistoricalCurrenciesList().pipe(
+          map((response) => {
+            return getHistoricalCurrenciesListSuccessAction({response})
+          }),
+          catchError((error: any) => of(getHistoricalCurrenciesListFailure(error))))
+      )
+    )
+  );
 
 
 
